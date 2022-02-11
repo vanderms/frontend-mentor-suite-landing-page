a330de1822139bf9154f52604241e5154a9a1099
vanderms <vanderms.84@gmail.com>
2022-02-11 12:57:55 -0300

    add footer cream background and links aria labels

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,37 +7,42 @@ import SVGIconTwitter from './svg-icon-twitter';
 export default function Footer() {
   return (
     <footer className="footer-root">
-      <img src={path('/assets/logo.svg')} alt="logo" className="logo" />
-      <div className="copyright">Copyright - Suite</div>
-      <ul className="social-links">
-        <li>
-          <a
-            href="https://pt-br.facebook.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SVGIconFacebook />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SVGIconTwitter />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <SVGIconIstagram />
-          </a>
-        </li>
-      </ul>
+      <div className="container">
+        <img src={path('/assets/logo.svg')} alt="logo" className="logo" />
+        <div className="copyright">Copyright - Suite</div>
+        <ul className="social-links">
+          <li>
+            <a
+              href="https://pt-br.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="facebook"
+            >
+              <SVGIconFacebook />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="twitter"
+            >
+              <SVGIconTwitter />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="instagram"
+            >
+              <SVGIconIstagram />
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
